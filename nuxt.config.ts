c9c33ffc1bf810064881656bb9fcca6dfc5ce39e
Daniele Bonfanti <bonfantidaniele94@gmail.com>
2022-08-30 21:54:37 +0200

    [INITIAL CONFIGURATION] Add chakraUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    modules: [
+        '@nuxtjs/supabase',
+        '@chakra-ui/nuxt',
+        '@nuxtjs/emotion'
+    ],
+    css: ['@/assets/main.css']
 })
